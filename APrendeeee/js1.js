752b736c3489064e04a69f7b047912720bd39212
Lucas Bruder <110999721+Bruder1z@users.noreply.github.com>
2024-09-09 14:08:20 -0300

    Add files via upload

---
+++ APrendeeee/js1.js
@@ -0,0 +1,55 @@
+// var agora = new Date()
+
+// var diaSem = agora.getDay()
+
+// diaSem = 0;
+
+// console.log(diaSem)
+// switch(diaSem){
+//     case 0 :
+//         console.log('domingo')
+//         break
+//     case 1: 
+//     console.log('segunda')
+//         break
+//     case 2 :
+//         console.log('terca')
+//         break
+//     case 3:
+//         console.log('quarta')
+//         break
+//     case 4:
+//         console.log('quinta')
+//         break
+//         case 5:
+//             console.log('sexta')
+//             break
+//         case 6:
+//             console.log('sabado')
+//             break
+//         default:
+//             console.log('erro')
+//             break
+
+// }
+
+// function parImpar(n){
+//     if(n%2 == 0){
+//         return 'par'
+//     }else{
+//         return 'impar'
+//     }
+// }
+
+// let res = parImpar(4);
+// console.log(res)
+
+// function soma(n1 = 0, n2= 0){
+//     return n1 + n2
+// }
+// console.log(soma(2,5))
+
+let v = function(x){
+    return x * 2
+}
+console.log(v(5))
